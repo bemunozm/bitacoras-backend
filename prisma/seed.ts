f9ce5a84a094887358b3e06a08327eedb6ae7ea0
bemunozm <be.munozmoya@gmail.com>
2025-02-03 11:27:55 -0300

    Feature: Reemplazar la seed de usuarios por la creación de enfermedades en el script de seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,47 +1,15 @@
 import { PrismaClient } from '@prisma/client';
-import { users } from './data/users';
-import { generateToken } from '../src/utils/token';
-import { hashPassword } from '../src/utils/auth';
+import { diseases } from './data/diseases';
 
 const prisma = new PrismaClient();
 
 async function main() {
   try {
 
-    let defaultRole = await prisma.roles.findFirst({
-        where: {
-            name: 'Usuario'
-        }
+    
+    await prisma.diseases.createMany({
+        data: diseases
     })
-
-    if(!defaultRole) {
-       defaultRole = await prisma.roles.create({
-            data: {
-                name: 'Usuario'
-            }
-        })
-    }
-
-    for (const user of users) {
-
-        const password = generateToken()
-        const hashedPassword = await hashPassword(password)
-
-        const createdUser = await prisma.users.create({
-            data: {
-                ...user,
-                password: hashedPassword,
-            }
-        });
-
-        // Insertar la relación en role_user
-        await prisma.role_user.create({
-            data: {
-                user_id: createdUser.id, // ID del usuario recién creado
-                role_id: defaultRole.id, // ID del rol por defecto
-            },
-        });
-    }
     
   } catch (error) {
     console.log(error)
