ef3bcce15b184e2384e02826eeeb40581b87aad2
bemunozm <be.munozmoya@gmail.com>
2025-02-10 12:24:56 -0300

    Feature: Cambiar proveedor de base de datos de PostgreSQL a MySQL y eliminar migraciones obsoletas

--- prisma/migrations/20250103162922_test/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "job_position" DROP NOT NULL,
-ALTER COLUMN "profile_image" DROP NOT NULL;
--- prisma/migrations/20250122160905_modify_colum_properties_participants/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "participants" ALTER COLUMN "age" DROP NOT NULL,
-ALTER COLUMN "birthdate" DROP NOT NULL,
-ALTER COLUMN "nationality" DROP NOT NULL;
